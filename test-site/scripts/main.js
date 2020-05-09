2049cd61bbd07373f7bc777f5c9845bd576add8b
Tallinnablue <65081933+Tallinnablue@users.noreply.github.com>
2020-05-09 11:52:49 -0400

    Add files via upload

---
+++ test-site/scripts/main.js
@@ -0,0 +1,30 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/144x144-01.png') {
+      myImage.setAttribute ('src','images/apple-touch-icon.png');
+    } else {
+      myImage.setAttribute ('src','images/144x144-01.png');
+    }
+}
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if(!myName || myName === null) {
+      setUserName();
+    } else {
+      localStorage.setItem('name', myName);
+      myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+    }
+  }
+  if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+  }
+  myButton.onclick = function() {
+    setUserName();
+  }
